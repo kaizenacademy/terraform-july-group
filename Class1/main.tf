c12857054be47fba44a72ce0a9b30fc10240cff0
kaizenacademy <kaizenacademy@gmail.com>
2023-09-23 18:08:58 +0000

    new files

---
+++ Class1/main.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user" "lb2" {
+  name = "hello"
+}
+
+resource "aws_iam_user" "lb1" {
+  name = "hello1"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb2.name,    # == hello
+    aws_iam_user.lb1.name,   # == helo1
+  ]
+
+  group = aws_iam_group.developers.name     # == developers
+}
